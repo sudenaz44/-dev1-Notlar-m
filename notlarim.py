b263b4bed1f89ac38a4712ceb9179aee48cb0b2a
SUDENAZ KILIÇ <120420389+sudenaz44@users.noreply.github.com>
2025-04-05 15:20:15 +0300

    Update notlarim.py

--- notlarim.py
+++ notlarim.py
@@ -1,1 +1,16 @@
-print("Selam kanka, bu ilk commit’im")
+def sinav_calisma_programi():
+    konular = ["Git nedir", "Commit nasıl atılır", "Branch ne işe yarar", "Merge çatışması nedir"]
+    gunler = ["Pazartesi", "Salı", "Çarşamba", "Perşembe"]
+
+    program = {}
+
+    for i in range(len(konular)):
+        if i < len(gunler):
+            program[gunler[i]] = konular[i]
+        else:
+            program["Ekstra Gün"] = konular[i]
+
+    for gun, konu in program.items():
+        print(f"{gun} günü şu çalışılacak: {konu}")
+
+sinav_calisma_programi()
